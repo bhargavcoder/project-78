3a7238d14a8314c3949b2cf4d35992b99381c8c1
bhargavcoder <94901458+bhargavcoder@users.noreply.github.com>
2022-06-02 11:57:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var images = "https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg ,"https://i.postimg.cc/bw5W5zSK/mother.jpg";]
+var names = ["Family Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"]; 
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = [images];
+      document.getElementById("family_member_image").src = updatedImage;w
+      document.getElementById("https://i.postimg.cc/MGn9GJXw/family.jpg").src = updatedImage;
+      document.getElementById("https://i.postimg.cc/qqyYvVbq/grandpa.jpg").src = updatedImage;
+      document.getElementById("https://i.postimg.cc/wjMnFtMX/father.jpg").src = updatedImage;
+      document.getElementById("https://i.postimg.cc/JnL6wtrd/sister.jpg").src = updatedImage;
+      document.getElementById("https://i.postimg.cc/bw5W5zSK/mother.jpg").src = updatedImage;
+
+
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = [names] ;
+    document.getElementById("family_member_name").innerHTML = names;     
+    document.getElementById("Ranbir Singh").innerHTML = updatedName;
+    document.getElementById("Diljeet Singh").innerHTML = updatedName;
+    document.getElementById("Rocky Singh").innerHTML = updatedName;
+    document.getElementById("Alia Singh").innerHTML = updatedName;
+    document.getElementById("Soni Singh").innerHTML = updatedName;
+}
